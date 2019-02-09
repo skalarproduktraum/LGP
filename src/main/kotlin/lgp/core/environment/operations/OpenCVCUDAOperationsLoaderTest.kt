bd729152ecd268779f729d91b5268e6da48b3f35
Ulrik Guenther <hello@ulrik.is>
2019-02-09 22:18:18 +0100

    Make OpenCVCUdaOperationsLoaderTest work.

--- src/main/kotlin/lgp/core/environment/operations/OpenCVCUDAOperationsLoaderTest.kt
+++ src/main/kotlin/lgp/core/environment/operations/OpenCVCUDAOperationsLoaderTest.kt
@@ -1,11 +1,9 @@
 package lgp.core.environment.operations
 
-import org.bytedeco.javacpp.opencv_core
-import org.bytedeco.javacpp.opencv_core.getTickCount
-import org.bytedeco.javacpp.opencv_core.getTickFrequency
-import org.bytedeco.javacpp.opencv_cudafilters
+import org.bytedeco.javacpp.*
+import org.bytedeco.javacpp.opencv_core.*
 import org.bytedeco.javacpp.opencv_highgui.*
-import org.bytedeco.javacpp.opencv_videoio
+import org.bytedeco.javacpp.opencv_imgproc.*
 import org.junit.Test
 
 /**
@@ -19,36 +17,62 @@ class OpenCVCUDAOperationsLoaderTest {
     fun load() {
     }
 
+    fun randomCircles(mat: Mat, rng: RNG, count: Int): Mat {
+        for(i in 0 until count) {
+            circle(
+                mat, Point(rng.uniform(0, mat.rows()), rng.uniform(0, mat.cols())),
+                rng.uniform(20, 50),
+                Scalar(rng.uniform(0.0, 255.0))
+            )
+        }
+
+        return mat
+    }
+
     @Test
     fun testCUDA() {
-        val gauss = opencv_cudafilters.createGaussianFilter(opencv_core.CV_8U, opencv_core.CV_8U, opencv_core.Size(3, 3), 0.5)
-        val laplace = opencv_cudafilters.createLaplacianFilter(opencv_core.CV_8U, opencv_core.CV_8U, 4, 2.0, opencv_core.BORDER_DEFAULT, opencv_core.Scalar(0.0))
+        val showWindows = System.getProperty("ShowCUDAWindows", "false").toBoolean()
+        val rng = opencv_core.RNG()
+        var frame = opencv_core.Mat(480, 640, CV_8U)
+        randomCircles(frame, rng, 10)
 
-        val frame = opencv_core.Mat()
-        val cap = opencv_videoio.VideoCapture()
-
-        cap.read(frame)
         val gpuImage = opencv_core.GpuMat(frame)
+
+        println("Creating filters")
+        val gauss = opencv_cudafilters.createGaussianFilter(opencv_core.CV_8U, opencv_core.CV_8U, opencv_core.Size(3, 3), 0.5)
+        val laplace = opencv_cudafilters.createLaplacianFilter(opencv_core.CV_8U, opencv_core.CV_8U, 3, 2.0, opencv_core.BORDER_DEFAULT, opencv_core.Scalar(0.0))
 
         val result = opencv_core.GpuMat()
         val final = opencv_core.GpuMat()
 
         var running = true
 
+        if(showWindows) {
+            namedWindow("input", WINDOW_NORMAL)
+            namedWindow("gauss", WINDOW_NORMAL)
+            namedWindow("laplace", WINDOW_NORMAL)
+        }
+
+        println("Running")
         while(running) {
             val start = getTickCount()
-            cap.read(frame)
+            randomCircles(frame, rng, rng.uniform(2, 16))
+
             if(frame.empty()) {
+                println("Frame is empty")
                 continue
             }
 
             gpuImage.upload(frame)
             gauss.apply(gpuImage, result)
             laplace.apply(result, final)
-
-            namedWindow("input", WINDOW_NORMAL)
-            namedWindow("gauss", WINDOW_NORMAL)
-            namedWindow("laplace", WINDOW_NORMAL)
+            gauss.apply(final, result)
+            gauss.apply(result, final)
+            laplace.apply(final, result)
+            gauss.apply(result, final)
+            opencv_cudaarithm.subtract(result, final, gpuImage)
+            opencv_cudaarithm.threshold(gpuImage, result, rng.uniform(0.0, 255.0), 255.0, THRESH_BINARY)
+            opencv_cudaarithm.absdiff(gpuImage, result, final)
 
             val finalLocal = opencv_core.Mat()
             val gaussLocal = opencv_core.Mat()
@@ -59,13 +83,15 @@ class OpenCVCUDAOperationsLoaderTest {
             val fps = getTickFrequency() / (getTickCount() - start)
             println("fps=$fps")
 
-            imshow("input", frame)
-            imshow("gauss", gaussLocal)
-            imshow("laplace", finalLocal)
+            if(showWindows) {
+                imshow("input", frame)
+                imshow("gauss", gaussLocal)
+                imshow("laplace", finalLocal)
 
-            val key = waitKey(30)
-            if(key == 27) {
-                running = false
+                val key = waitKey(30)
+                if (key == 27) {
+                    running = false
+                }
             }
         }
 
